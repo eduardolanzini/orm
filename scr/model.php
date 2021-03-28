d114786b5268189ac54f2521112749fed8bcb6b9
Eduardo <eduardolanzini91@gmail.com>
2021-03-28 05:34:33 -0300

    Correções

--- scr/model.php
+++ scr/model.php
@@ -2,42 +2,58 @@
 
 namespace EduardoLanzini;
 
+use EduardoLanzini\DB;
+
 Class Model
 {
-	public function getTable()
-	{
-		return static::$table;
-	}
+    public function getTable()
+    {
+        return static::$table;
+    }
 
-	public function getPrimaryKey()
-	{
-		return static::$primaryKey;
-	}
+    public function getPrimaryKey()
+    {
+        return static::$primaryKey;
+    }
 
-	public static function get($id)
-	{
-		return DB::table(static::$table.(static::$alias ? ' as '.static::$alias : '') )
-		->where((static::$alias ? static::$alias.'.' : '').static::$primaryKey, $id)
-		->get();
-	}
+    public static function get($id)
+    {
+        return DB::table(static::$table.(static::$alias ? ' as '.static::$alias : '') )
+        ->where((static::$alias ? static::$alias.'.' : '').static::$primaryKey, $id)
+        ->get();
+    }
+
+    public static function find($value,$column=null)
+    {
+        $v = isset($column) ? $column : $value;
+        $c = isset($column) ? $value  : static::$primaryKey;
+
+        return DB::table(static::$table.(static::$alias ? ' as '.static::$alias : '') )
+        ->find($c,$v);
+    }
+
+    public static function select()
+    {
+        return DB::table(static::$table.(static::$alias ? ' as '.static::$alias : '') );
+    }
 
     public static function all()
     {
-    	return DB::table(static::$table)->all();
+        return DB::table(static::$table)->all();
     }
 
-    protected static function create($fields)
+    public static function create($fields)
     {
-    	return DB::table(static::$table)->insert($fields);
+        return DB::table(static::$table)->insert($fields);
     }
 
-    protected static function update($id, $fields)
+    public static function update($id, $fields)
     {
-    	return DB::table(static::$table)->where(static::$primaryKey, $id)->update($fields);
+        return DB::table(static::$table)->where(static::$primaryKey, $id)->update($fields);
     }
 
-    protected static function delete($id)
+    public static function delete($id)
     {
-    	return DB::table(static::$table)->where(static::$primaryKey, $id)->delete();
+        return DB::table(static::$table)->where(static::$primaryKey, $id)->delete();
     }
 }
